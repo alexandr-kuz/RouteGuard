7521efc3875f0d93d1a733a10d22a480fd24a5ff
alexandr-kuz <kavboy2006@gmail.com>
2026-02-24 17:39:00 +0300

    1

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -37,7 +37,7 @@ const router = createRouter({
 })
 
 // Auth guard
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _, next) => {
   const token = localStorage.getItem('rg_token')
   
   if (to.meta.requiresAuth && !token) {
