7052076000358c57e3723f1f9ebcc44c04191934
alexandr-kuz <kavboy2006@gmail.com>
2026-02-24 13:30:27 +0300

    chore: bump version to 0.2.1

--- backend/main.go
+++ backend/main.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	appName    = "routeguard"
-	appVersion = "0.2.0"
+	appVersion = "0.2.1"
 )
 
 // getDefaultConfigPath возвращает путь к конфигурации по умолчанию для текущей ОС
